3450697e8aeb742280667ed60624fe6da1d5ca05
xinyanzu1 <45288460+xinyanzu1@users.noreply.github.com>
2019-05-01 14:52:24 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Prince Al's Royal Hotel</title>
+    <link type='text/css' rel='stylesheet' href='login.css'>
+</head>
+<body>
+<form action="" method="post" class="tabele">
+    <input type="email" name="" value="" class="na">
+    <input type="password" name="" value="" class="pw">
+    <input type="submit" name="" value="submit" class="sub">
+</form>
+</body>
+</html>
